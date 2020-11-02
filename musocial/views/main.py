a4d08d1a1ba7736a594925b079bc6bb37309ef8a
Ionuț Bizău <i@ibz.me>
2020-11-02 15:34:36 +0800

    Add sitemap and fix robotstxt order.

--- musocial/views/main.py
+++ musocial/views/main.py
@@ -4,9 +4,19 @@ from flask_jwt_extended.exceptions import NoAuthorizationError
 
 main_blueprint = Blueprint('main', __name__)
 
+@main_blueprint.route('/sitemap.xml', methods=['GET'])
+def sitemap_xml():
+    return Response("""<?xml version="1.0" encoding="UTF-8"?>
+        <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+            <url><loc>http://musocial.me/about</loc></url>
+        </urlset>""", mimetype='application/xml')
+
 @main_blueprint.route('/robots.txt', methods=['GET'])
 def robots_txt():
-    return Response("User-agent: *\nDisallow: /\nAllow: /about\n", mimetype='text/plain')
+    return Response("""User-agent: *
+Allow: /about
+Disallow: /
+""", mimetype='text/plain')
 
 @main_blueprint.route('/', methods=['GET'])
 def index():
